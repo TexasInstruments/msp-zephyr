3fdf026ac3dfafa5bbcff49955457f34da49d235
Santhosh Charles <santhosh@linumiz.com>
2025-08-13 18:43:12 +0530

    drivers: spi: Add driver support for TI MSPM0 SPI module
    Add support for the SPI module on TI’s MSPM0 MCUs.
    The driver supports master mode transfers with configurable
    frame size (4–16 bits), clock polarity/phase, bit order.
    
    Signed-off-by: Santhosh Charles <santhosh@linumiz.com>
    Signed-off-by: Jackson Farley <j-farley@ti.com>
    Co-authored-by: Hans Binderup <habi@bang-olufsen.dk>

--- drivers/spi/spi_mspm0.c
+++ drivers/spi/spi_mspm0.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2024 Bang & Olufsen A/S, Denmark
+ * Copyright (c) 2025 Linumiz GmbH
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -7,250 +8,283 @@
 #define DT_DRV_COMPAT ti_mspm0_spi
 
 #include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/drivers/clock_control/mspm0_clock_control.h>
 #include <zephyr/drivers/pinctrl.h>
 #include <zephyr/drivers/spi.h>
+#include <zephyr/irq.h>
 #include <zephyr/logging/log.h>
-#include <soc.h>
 
-/* DriverLib includes */
+/* TI DriverLib includes */
 #include <driverlib/dl_spi.h>
 
 LOG_MODULE_REGISTER(spi_mspm0, CONFIG_SPI_LOG_LEVEL);
 
-/* must be included after log module registration */
+/* This must be included after log module registration */
 #include "spi_context.h"
 
-#define SPI_MODE(operation) (operation & BIT(0) ? DL_SPI_MODE_PERIPHERAL : DL_SPI_MODE_CONTROLLER)
-#define BIT_ORDER_MODE(operation)                                                                  \
-	(operation & BIT(4) ? DL_SPI_BIT_ORDER_LSB_FIRST : DL_SPI_BIT_ORDER_MSB_FIRST)
-#define DATA_SIZE_MODE(operation) (SPI_WORD_SIZE_GET(operation) - 1)
+/* Data Frame Size (DFS) */
+#define SPI_DFS_8BIT		1
+#define SPI_DFS_16BIT		2
 
-#define POLARITY_MODE(operation)                                                                   \
+/* Range for SPI Serial Clock Rate (SCR) */
+#define MSPM0_SPI_SCR_MIN	0
+#define MSPM0_SPI_SCR_MAX	1023
+
+/* Delay after enabling power for SPI module */
+#define POWER_STARTUP_DELAY	16
+
+#define SPI_DT_CLK_DIV(inst)									\
+	DT_INST_PROP(inst, ti_clk_div)
+
+#define SPI_DT_CLK_DIV_ENUM(inst)								\
+	_CONCAT(DL_SPI_CLOCK_DIVIDE_RATIO_, SPI_DT_CLK_DIV(inst))
+
+#define SPI_MODE(operation)									\
+	(operation & BIT(0) ? DL_SPI_MODE_PERIPHERAL : DL_SPI_MODE_CONTROLLER)
+
+#define BIT_ORDER_MODE(operation)								\
+	(operation & BIT(4) ? DL_SPI_BIT_ORDER_LSB_FIRST : DL_SPI_BIT_ORDER_MSB_FIRST)
+
+/* MSPM0 DSS field expects word size - 1 */
+#define DATA_SIZE_MODE(operation)								\
+	(SPI_WORD_SIZE_GET(operation) - 1)
+
+#define POLARITY_MODE(operation)								\
 	(SPI_MODE_GET(operation) & SPI_MODE_CPOL ? SPI_CTL0_SPO_HIGH : SPI_CTL0_SPO_LOW)
-#define PHASE_MODE(operation)                                                                      \
+
+#define PHASE_MODE(operation)									\
 	(SPI_MODE_GET(operation) & SPI_MODE_CPHA ? SPI_CTL0_SPH_SECOND : SPI_CTL0_SPH_FIRST)
-#define DUPLEX_MODE(operation)                                                                     \
+
+#define DUPLEX_MODE(operation)									\
 	(operation & BIT(11) ? SPI_CTL0_FRF_MOTOROLA_3WIRE : SPI_CTL0_FRF_MOTOROLA_4WIRE)
 
-/* Only motorola format requires config - TI format is a single value */
-#define FRAME_FORMAT_MODE(operation)                                                               \
-	(operation & SPI_FRAME_FORMAT_TI                                                           \
-		 ? SPI_CTL0_FRF_TI_SYNC                                                            \
-		 : DUPLEX_MODE(operation) | POLARITY_MODE(operation) | PHASE_MODE(operation))
+/* TI uses fixed frame format; Motorola combines duplex, polarity, phase */
+#define FRAME_FORMAT_MODE(operation)								\
+	(operation & SPI_FRAME_FORMAT_TI							\
+		? SPI_CTL0_FRF_TI_SYNC								\
+		: DUPLEX_MODE(operation) | POLARITY_MODE(operation) | PHASE_MODE(operation))
 
-/* 0x38 represents the bits 8, 16 and 32. Knowing that 24 is bits 8 and 16
- * These are the bits were when you divide by 8, you keep the result as it is.
- * For all the other ones, 4 to 7, 9 to 15, etc... you need a +1,
- * since on such division it takes only the result above 0
+/* Computes the minimum number of bytes per frame using word_size. Utilizes ceil
+ * division, to ensure sufficient byte allocation for non-multiples of 8 bits
  */
-#define BYTES_PER_FRAME(word_size)                                                                 \
-	(((word_size) & ~0x38) ? (((word_size) / 8) + 1) : ((word_size) / 8))
+#define BYTES_PER_FRAME(word_size) (((word_size) + 7) / 8)
 
 struct spi_mspm0_config {
-	SPI_Regs *base;
+	SPI_Regs *spi_base;
 	const struct pinctrl_dev_config *pinctrl;
+
 	const DL_SPI_ClockConfig clock_config;
+	const struct mspm0_sys_clock *clock_subsys;
+	void (*irq_config_func)(const struct device *dev);
 };
 
 struct spi_mspm0_data {
-	struct spi_context ctx;
-	uint8_t dfs;
+	struct spi_context spi_ctx;
+	struct k_sem spi_idle;
 };
 
-static int spi_mspm0_configure(const struct device *dev, const struct spi_config *spi_cfg)
+static void spi_mspm0_isr(const struct device *dev)
 {
-	struct spi_mspm0_data *const data = dev->data;
-	const struct spi_mspm0_config *const cfg = dev->config;
-	struct spi_context *ctx = &data->ctx;
-	const struct device *const clk_dev = DEVICE_DT_GET(DT_NODELABEL(clkmux));
-	uint32_t clockRate;
+	const struct spi_mspm0_config *config = dev->config;
+	struct spi_mspm0_data *data = dev->data;
 
-	ret = clock_control_get_rate(clk_dev, (clock_control_subsys_t)config->clock_subsys, &clockRate);
-
-	if(ret < 0){
-		return -ENODEV;
+	if (DL_SPI_getPendingInterrupt(config->spi_base) == DL_SPI_IIDX_IDLE) {
+		DL_SPI_disableInterrupt(config->spi_base, DL_SPI_INTERRUPT_IDLE);
+		k_sem_give(&data->spi_idle);
 	}
+}
 
-	if (spi_context_configured(ctx, spi_cfg)) {
-		/* this configuration is already in use */
+static int spi_mspm0_configure(const struct device *dev, const struct spi_config *spi_cfg,
+			       uint8_t dfs)
+{
+	const struct spi_mspm0_config *const config = dev->config;
+	struct spi_mspm0_data *const data = dev->data;
+	const struct device *clk_dev = DEVICE_DT_GET(DT_NODELABEL(ckm));
+
+	uint32_t clock_rate;
+	uint16_t clock_scr;
+	int ret;
+
+	if (spi_context_configured(&data->spi_ctx, spi_cfg)) {
+		/* This configuration is already in use */
 		return 0;
 	}
 
-	if (SPI_MODE(spi_cfg->operation) == DL_SPI_MODE_PERIPHERAL) {
-		/* todo: not yet tested so don't add support yet */
+	/* Only master mode is supported */
+	if (SPI_OP_MODE_GET(spi_cfg->operation) != SPI_OP_MODE_MASTER) {
 		return -ENOTSUP;
 	}
 
-	if (spi_cfg->frequency > (clockRate / 2)) {
+	ret = clock_control_get_rate(clk_dev, (struct mspm0_sys_clock *)config->clock_subsys,
+				     &clock_rate);
+	if (ret < 0) {
+		return ret;
+	}
+
+	if (spi_cfg->frequency > (clock_rate / 2)) {
 		return -EINVAL;
 	}
 
-	/* see DL_SPI_setBitRateSerialClockDivider for details */
-	uint16_t clock_scr = clockRate / ((2 * spi_cfg->frequency) - 1);
-
-	if (!IN_RANGE(clock_scr, 0, 1023)) {
+	/* See DL_SPI_setBitRateSerialClockDivider for details */
+	clock_scr = (clock_rate / (2 * spi_cfg->frequency)) - 1;
+	if (!IN_RANGE(clock_scr, MSPM0_SPI_SCR_MIN, MSPM0_SPI_SCR_MAX)) {
 		return -EINVAL;
 	}
 
-	const DL_SPI_Config dl_cfg = {
+	const DL_SPI_Config dl_spi_cfg = {
+		.parity = DL_SPI_PARITY_NONE,			/* Currently unused in zephyr */
+		.chipSelectPin = DL_SPI_CHIP_SELECT_NONE,	/* spi_context controls the CS */
 		.mode = SPI_MODE(spi_cfg->operation),
-		.frameFormat = FRAME_FORMAT_MODE(spi_cfg->operation),
-		.chipSelectPin = DL_SPI_CHIP_SELECT_NONE, /* spi_context controls the CS pin */
-		.parity = DL_SPI_PARITY_NONE,             /* currently unused in zephyr */
 		.bitOrder = BIT_ORDER_MODE(spi_cfg->operation),
 		.dataSize = DATA_SIZE_MODE(spi_cfg->operation),
+		.frameFormat = FRAME_FORMAT_MODE(spi_cfg->operation),
 	};
 
-	/* peripheral should always be disabled prior to applying a new configuration */
-	DL_SPI_disable(cfg->base);
-	DL_SPI_init(cfg->base, (DL_SPI_Config *)&dl_cfg);
-	DL_SPI_setBitRateSerialClockDivider(cfg->base, (uint32_t)clock_scr);
+	/* Peripheral should be disabled before applying a new configuration */
+	DL_SPI_disable(config->spi_base);
 
-	data->dfs = BYTES_PER_FRAME(SPI_WORD_SIZE_GET(spi_cfg->operation));
-	if (data->dfs > 2) {
-		DL_SPI_enablePacking(cfg->base);
+	DL_SPI_init(config->spi_base, (DL_SPI_Config *)&dl_spi_cfg);
+	DL_SPI_setBitRateSerialClockDivider(config->spi_base, (uint32_t)clock_scr);
+
+	if (dfs > SPI_DFS_16BIT) {
+		DL_SPI_enablePacking(config->spi_base);
 	} else {
-		DL_SPI_disablePacking(cfg->base);
+		DL_SPI_disablePacking(config->spi_base);
 	}
 
 	if (SPI_MODE_GET(spi_cfg->operation) & SPI_MODE_LOOP) {
-		DL_SPI_enableLoopbackMode(cfg->base);
+		DL_SPI_enableLoopbackMode(config->spi_base);
 	} else {
-		DL_SPI_disableLoopbackMode(cfg->base);
+		DL_SPI_disableLoopbackMode(config->spi_base);
 	}
 
-	DL_SPI_enable(cfg->base);
+	DL_SPI_enable(config->spi_base);
 
-	/* save config so it can be reused.
-	 * also it's required for the lock owner to work
-	 */
-	ctx->config = spi_cfg;
+	/* Cache SPI config for reuse, required by spi_context owner */
+	data->spi_ctx.config = spi_cfg;
 
 	return 0;
 }
 
-static bool spi_mspm0_transfer_ongoing(struct spi_context *ctx)
+static void spi_mspm0_frame_tx(const struct device *dev, uint8_t dfs)
 {
-	return spi_context_tx_on(ctx) || spi_context_rx_on(ctx);
-}
-
-static void spi_mspm0_frame_tx(const struct device *dev)
-{
+	const struct spi_mspm0_config *config = dev->config;
 	struct spi_mspm0_data *data = dev->data;
-	const struct spi_mspm0_config *cfg = dev->config;
-	struct spi_context *ctx = &data->ctx;
 
-	/* nop tx if no data is expected */
+	/* Transmit dummy frame when no TX data is provided */
 	uint32_t tx_frame = 0;
 
-	if (spi_context_tx_buf_on(ctx)) {
-		if (data->dfs == 1) {
-			tx_frame = UNALIGNED_GET((uint8_t *)(ctx->tx_buf));
-		} else if (data->dfs == 2) {
-			tx_frame = UNALIGNED_GET((uint16_t *)(ctx->tx_buf));
+	k_sem_reset(&data->spi_idle);
+
+	if (spi_context_tx_buf_on(&data->spi_ctx)) {
+		if (dfs == SPI_DFS_8BIT) {
+			tx_frame = UNALIGNED_GET((uint8_t *)(data->spi_ctx.tx_buf));
+		} else if (dfs == SPI_DFS_16BIT) {
+			tx_frame = UNALIGNED_GET((uint16_t *)(data->spi_ctx.tx_buf));
 		} else {
-			tx_frame = UNALIGNED_GET((uint32_t *)(ctx->tx_buf));
+			tx_frame = UNALIGNED_GET((uint32_t *)(data->spi_ctx.tx_buf));
 		}
 	}
+	DL_SPI_transmitDataCheck32(config->spi_base, tx_frame);
 
-	DL_SPI_transmitDataBlocking32(cfg->base, tx_frame);
-	while (DL_SPI_isBusy(cfg->base)) {
-		/* wait for tx fifo to be sent */
-	}
+	DL_SPI_enableInterrupt(config->spi_base, DL_SPI_INTERRUPT_IDLE);
+	k_sem_take(&data->spi_idle, K_FOREVER);
 
-	spi_context_update_tx(ctx, data->dfs, 1);
+	spi_context_update_tx(&data->spi_ctx, dfs, 1);
 }
 
-static void spi_mspm0_frame_rx(const struct device *dev)
+static void spi_mspm0_frame_rx(const struct device *dev, uint8_t dfs)
 {
+	const struct spi_mspm0_config *config = dev->config;
 	struct spi_mspm0_data *data = dev->data;
-	const struct spi_mspm0_config *cfg = dev->config;
-	struct spi_context *ctx = &data->ctx;
+	uint32_t rx_val = 0;
 
-	const uint32_t rx_frame = DL_SPI_receiveDataBlocking32(cfg->base);
+	DL_SPI_receiveDataCheck32(config->spi_base, &rx_val);
 
-	/* only update rx buffer if the context is configured to do so
-	 * could be eg. if a write is triggered without a read - here the peripheral would not
-	 * respond why stale POCI would result in 0 being read out (full duplex mode)
-	 */
-	if (!spi_context_rx_buf_on(ctx)) {
+	if (!spi_context_rx_buf_on(&data->spi_ctx)) {
 		return;
 	}
 
-	if (data->dfs == 1) {
-		UNALIGNED_PUT(rx_frame, (uint8_t *)ctx->rx_buf);
-	} else if (data->dfs == 2) {
-		UNALIGNED_PUT(rx_frame, (uint16_t *)ctx->rx_buf);
+	if (dfs == SPI_DFS_8BIT) {
+		UNALIGNED_PUT((uint8_t)rx_val, (uint8_t *)data->spi_ctx.rx_buf);
+	} else if (dfs == SPI_DFS_16BIT) {
+		UNALIGNED_PUT((uint16_t)rx_val, (uint16_t *)data->spi_ctx.rx_buf);
 	} else {
-		UNALIGNED_PUT(rx_frame, (uint32_t *)ctx->rx_buf);
+		UNALIGNED_PUT(rx_val, (uint32_t *)data->spi_ctx.rx_buf);
 	}
 
-	spi_context_update_rx(ctx, data->dfs, 1);
+	spi_context_update_rx(&data->spi_ctx, dfs, 1);
 }
 
-static void spi_mspm0_start_transfer(const struct device *dev)
+static void spi_mspm0_start_transfer(const struct device *dev, uint8_t dfs)
 {
 	struct spi_mspm0_data *data = dev->data;
-	struct spi_context *ctx = &data->ctx;
 
-	spi_context_cs_control(ctx, true);
+	spi_context_cs_control(&data->spi_ctx, true);
 
-	while (spi_mspm0_transfer_ongoing(ctx)) {
-		spi_mspm0_frame_tx(dev);
-		spi_mspm0_frame_rx(dev);
+	while (spi_context_tx_on(&data->spi_ctx) || spi_context_rx_on(&data->spi_ctx)) {
+		spi_mspm0_frame_tx(dev, dfs);
+		spi_mspm0_frame_rx(dev, dfs);
 	}
 
-	spi_context_cs_control(ctx, false);
-	spi_context_complete(ctx, dev, 0);
+	spi_context_cs_control(&data->spi_ctx, false);
+	spi_context_complete(&data->spi_ctx, dev, 0);
 }
 
-static int spi_mspm0_transceive(const struct device *dev, const struct spi_config *spi_cfg,
+static int spi_mspm0_transceive(const struct device *dev,
+				const struct spi_config *spi_cfg,
 				const struct spi_buf_set *tx_bufs,
 				const struct spi_buf_set *rx_bufs)
 {
 	struct spi_mspm0_data *data = dev->data;
-	struct spi_context *ctx = &data->ctx;
+	int ret;
+	uint8_t dfs;
 
-	spi_context_lock(ctx, false, NULL, NULL, spi_cfg);
+	if (!tx_bufs && !rx_bufs) {
+		return 0;
+	}
 
-	int ret = spi_mspm0_configure(dev, spi_cfg);
+	spi_context_lock(&data->spi_ctx, false, NULL, NULL, spi_cfg);
 
+	dfs = BYTES_PER_FRAME(SPI_WORD_SIZE_GET(spi_cfg->operation));
+
+	ret = spi_mspm0_configure(dev, spi_cfg, dfs);
 	if (ret != 0) {
-		spi_context_release(ctx, ret);
+		spi_context_release(&data->spi_ctx, ret);
 		return ret;
 	}
 
-	spi_context_buffers_setup(ctx, tx_bufs, rx_bufs, data->dfs);
-	spi_mspm0_start_transfer(dev);
+	spi_context_buffers_setup(&data->spi_ctx, tx_bufs, rx_bufs, dfs);
 
-	ret = spi_context_wait_for_completion(ctx);
-	spi_context_release(ctx, ret);
+	spi_mspm0_start_transfer(dev, dfs);
+
+	ret = spi_context_wait_for_completion(&data->spi_ctx);
+	spi_context_release(&data->spi_ctx, ret);
 
 	return ret;
 }
 
-static int spi_mspm0_release(const struct device *dev, const struct spi_config *config)
+static int spi_mspm0_release(const struct device *dev, const struct spi_config *spi_cfg)
 {
+	const struct spi_mspm0_config *config = dev->config;
 	struct spi_mspm0_data *data = dev->data;
-	const struct spi_mspm0_config *cfg = dev->config;
-	struct spi_context *ctx = &data->ctx;
 
-	if (!spi_context_configured(ctx, config)) {
+	if (!spi_context_configured(&data->spi_ctx, spi_cfg)) {
 		return -EINVAL;
 	}
 
-	if (DL_SPI_isBusy(cfg->base)) {
+	if (DL_SPI_isBusy(config->spi_base)) {
 		return -EBUSY;
 	}
 
-	spi_context_unlock_unconditionally(ctx);
+	spi_context_unlock_unconditionally(&data->spi_ctx);
 	return 0;
 }
 
 static const struct spi_driver_api spi_mspm0_api = {
 	.transceive = spi_mspm0_transceive,
-	.release = spi_mspm0_release,
+	.release    = spi_mspm0_release,
 };
 
 static int spi_mspm0_init(const struct device *dev)
@@ -258,48 +292,60 @@ static int spi_mspm0_init(const struct device *dev)
 	struct spi_mspm0_data *data = dev->data;
 	const struct spi_mspm0_config *cfg = dev->config;
 	struct spi_context *ctx = &data->ctx;
-
-	DL_SPI_reset(cfg->base);
-	DL_SPI_enablePower(cfg->base);
+	DL_SPI_enablePower(config->spi_base);
 	delay_cycles(POWER_STARTUP_DELAY);
 
-	int ret = pinctrl_apply_state(cfg->pinctrl, PINCTRL_STATE_DEFAULT);
-
-	if (ret < 0) {
-		LOG_ERR("Failed to apply pinctrl, err: %d", ret);
-		return ret;
-	}
-
-	ret = spi_context_cs_configure_all(ctx);
+	ret = pinctrl_apply_state(config->pinctrl, PINCTRL_STATE_DEFAULT);
 	if (ret < 0) {
 		return ret;
 	}
 
-	DL_SPI_setClockConfig(cfg->base, (DL_SPI_ClockConfig *)&cfg->clock_config);
-	DL_SPI_enable(cfg->base);
+	ret = spi_context_cs_configure_all(&data->spi_ctx);
+	if (ret < 0) {
+		return ret;
+	}
+	DL_SPI_setClockConfig(config->spi_base, (DL_SPI_ClockConfig *)&config->clock_config);
+	DL_SPI_enable(config->spi_base);
 
-	spi_context_unlock_unconditionally(ctx);
+	spi_context_unlock_unconditionally(&data->spi_ctx);
+
+	config->irq_config_func(dev);
 
 	return ret;
 }
 
-#define MSPM0_SPI_INIT(inst)                                                                       \
-	PINCTRL_DT_INST_DEFINE(inst);                                                              \
-                                                                                                   \
-	static struct spi_mspm0_config spi_mspm0_##inst##_cfg = {                                  \
-		.base = (SPI_Regs *)DT_INST_REG_ADDR(inst),                                        \
-		.pinctrl = PINCTRL_DT_INST_DEV_CONFIG_GET(inst),                                   \
-		.clock_config = {.clockSel = (DT_INST_CLOCKS_CELL(index, bus) & MSPM0_CLOCK_SEL_MASK),                                  \
-				 .divideRatio = DL_SPI_CLOCK_DIVIDE_RATIO_1},                      \
-	};                                                                                         \
-                                                                                                   \
-	static struct spi_mspm0_data spi_mspm0_##inst##_data = {                                   \
-		SPI_CONTEXT_INIT_LOCK(spi_mspm0_##inst##_data, ctx),                               \
-		SPI_CONTEXT_INIT_SYNC(spi_mspm0_##inst##_data, ctx),                               \
-		SPI_CONTEXT_CS_GPIOS_INITIALIZE(DT_DRV_INST(inst), ctx)};                          \
-                                                                                                   \
-	DEVICE_DT_INST_DEFINE(inst, spi_mspm0_init, NULL, &spi_mspm0_##inst##_data,                \
-			      &spi_mspm0_##inst##_cfg, POST_KERNEL, CONFIG_SPI_INIT_PRIORITY,      \
+#define MSPM0_SPI_INIT(inst)									\
+	PINCTRL_DT_INST_DEFINE(inst);								\
+												\
+	static void spi_mspm0_irq_config_##inst(const struct device *dev)			\
+	{											\
+		IRQ_CONNECT(DT_INST_IRQN(inst), DT_INST_IRQ(inst, priority), spi_mspm0_isr,	\
+				DEVICE_DT_INST_GET(inst), 0);					\
+		irq_enable(DT_INST_IRQN(inst));							\
+	};											\
+												\
+	static const struct mspm0_sys_clock mspm0_spi_sys_clock##inst =				\
+		MSPM0_CLOCK_SUBSYS_FN(inst);							\
+												\
+	static struct spi_mspm0_config spi_mspm0_config_##inst = {				\
+		.spi_base = (SPI_Regs *)DT_INST_REG_ADDR(inst),					\
+		.pinctrl = PINCTRL_DT_INST_DEV_CONFIG_GET(inst),				\
+		.clock_config = {.clockSel =							\
+				  MSPM0_CLOCK_PERIPH_REG_MASK(DT_INST_CLOCKS_CELL(inst, clk)),  \
+				 .divideRatio = SPI_DT_CLK_DIV_ENUM(inst)},			\
+		.clock_subsys = &mspm0_spi_sys_clock##inst,					\
+		.irq_config_func = spi_mspm0_irq_config_##inst,					\
+	};											\
+												\
+	static struct spi_mspm0_data spi_mspm0_data_##inst = {					\
+		.spi_idle = Z_SEM_INITIALIZER(spi_mspm0_data_##inst.spi_idle, 0, 1),		\
+		SPI_CONTEXT_INIT_LOCK(spi_mspm0_data_##inst, spi_ctx),				\
+		SPI_CONTEXT_INIT_SYNC(spi_mspm0_data_##inst, spi_ctx),				\
+		SPI_CONTEXT_CS_GPIOS_INITIALIZE(DT_DRV_INST(inst), spi_ctx)			\
+	};											\
+												\
+	DEVICE_DT_INST_DEFINE(inst, spi_mspm0_init, NULL, &spi_mspm0_data_##inst,		\
+			      &spi_mspm0_config_##inst, POST_KERNEL, CONFIG_SPI_INIT_PRIORITY,	\
 			      &spi_mspm0_api);
 
 DT_INST_FOREACH_STATUS_OKAY(MSPM0_SPI_INIT)
