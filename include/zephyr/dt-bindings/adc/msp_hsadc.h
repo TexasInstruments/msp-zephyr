e953fa72456d63787c0d17fef58b19885cb58ade
Aman Lachhiramka <a-lachhiramka@ti.com>
2025-09-26 18:32:33 +0530

    drivers: adc: add TI MSPM33 HSADC driver
    Add initial support for MSPM33 HSADC driver
    
    Signed-off-by: Aman Lachhiramka <a-lachhiramka@ti.com>

---
+++ include/zephyr/dt-bindings/adc/msp_hsadc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Texas Instruments
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _MSP_DT_BINDINGS_ADC_H_
+#define _MSP_DT_BINDINGS_ADC_H_
+
+#include <zephyr/dt-bindings/adc/adc.h>
+
+#endif /* _MSP_DT_BINDINGS_ADC_H_ */
