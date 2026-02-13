01998f9c563b47b5715fa994a965023bd51ed801
Tejas Garg <t-garg@ti.com>
2026-02-13 17:17:59 +0530

    boards: ti: lp_mspm33c321a: add OpenOCD support and enable XIP
    Add OpenOCD flash runner support for the LP_MSPM33C321A board:
    - Configure board.cmake with OpenOCD runner arguments
    - Add OpenOCD configuration file using XDS110 debug probe
    
    Additionally:
    - Enable XIP (execute in place) by removing CONFIG_XIP=n
    - Fix flash size from 1000 to 1024 KB in board YAML
    
    Signed-off-by: Tejas Garg <t-garg@ti.com>

--- boards/ti/lp_mspm33c321a/board.cmake
+++ boards/ti/lp_mspm33c321a/board.cmake
@@ -1,2 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(openocd
+  "--openocd=${ZEPHYR_BASE}/../openocd/src/openocd"
+  "--openocd-search=${ZEPHYR_BASE}/../openocd/tcl"
+)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
