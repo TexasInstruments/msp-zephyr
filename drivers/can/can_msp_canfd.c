93901c8fe6c4ba19a6ef462a81e2c4a9b0025584
Aman Lachhiramka <a-lachhiramka@ti.com>
2026-03-20 14:55:15 +0530

    drivers: can: update device trees to ti,msp-canfd
    Update CAN binding and device trees to use the generic ti,msp-canfd
    compatible string instead of ti,mspm0-canfd. Fix clock references
    to use MSP_CLOCK_CANCLK and correct syspll reference to syspll2x.
    
    Signed-off-by: Aman Lachhiramka <a-lachhiramka@ti.com>

--- drivers/can/can_msp_canfd.c
+++ drivers/can/can_msp_canfd.c
@@ -19,7 +19,7 @@
 
 LOG_MODULE_REGISTER(can_msp_canfd, CONFIG_CAN_LOG_LEVEL);
 
-#define DT_DRV_COMPAT ti_mspm0_canfd
+#define DT_DRV_COMPAT ti_msp_canfd
 
 #define MSP_MCAN_REVID_SCHEME_INVALID		0x00
 #define MSP_MCAN_MRBA				0x8000
