618612e5b957afe0e48ba55699edf6458e5afcd7
Jackson Farley <j-farley@ti.com>
2026-06-12 16:13:03 -0500

    syspll workaround added

--- include/zephyr/drivers/clock_control/mspm0_clock_control.h
+++ include/zephyr/drivers/clock_control/mspm0_clock_control.h
@@ -15,4 +15,17 @@ struct mspm0_sys_clock {
 
 #define MSPM0_CLOCK_SUBSYS_FN(index) {.clk = DT_INST_CLOCKS_CELL(index, clk)}
 
+/* function disabling syspll and returning MCLK to SYSOSC (if so configured) */
+void mspm0_switch_and_disable_syspll(void);
+
+void mspm0_enable_syspll(void);
+
+/* function implementing the workaround to add SYSPLL and verify that the
+ * frequency lock has been correctly achieved.
+ * Also switches MCLK to SYSPLL if configured that way in the devicetree
+ */
+void mspm0_verify_syspll(void);
+
+void mspm0_switch_to_syspll(void);
+
 #endif /* ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_MSPM0_CLOCK_CONTROL */
